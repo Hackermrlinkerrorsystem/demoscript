e49e5af66fb93b4da87b3936a96352bf7cfc54dd
MRLINKERRORSYSTEM <54674143+Hackermrlinkerrorsystem@users.noreply.github.com>
2019-09-01 11:58:11 +0700

    Create restart.py

---
+++ restart.py
@@ -0,0 +1,24 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+mengetik('menghubungi Mr MRLINKERRORSYSTEM..')
+mengetik('.')
+mengetik('.')
+mengetik('mencoba meminta akses🤡..')
+mengetik('...')
+mengetik('...')
+mengetik('...')
+mengetik('...')
+mengetik('...')
+mengetik('...')
+mengetik('...')
+mengetik('mengupdate!')
+mengetik('AdminCantikpro😃')
+mengetik('100%')
+mengetik ('https://mrlinkerrorsystem.blogspot.com')
+mengetik ('Harus salin dulu text diatas biar berkah hehehe 😘')
